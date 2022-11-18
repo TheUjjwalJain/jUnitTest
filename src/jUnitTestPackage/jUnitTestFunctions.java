63d3975e820a2416871c3462ec1b3dd3c503cee3
ujjwaljain <ujjwaljain@localhost>
2022-11-18 12:32:06 +0530

    test

---
+++ src/jUnitTestPackage/jUnitTestFunctions.java
@@ -0,0 +1,11 @@
+package jUnitTestPackage;
+
+public class jUnitTestFunctions {
+	public int addNumbers(int num1, int num2){
+		return num1 + num2;
+	}
+	
+	public String addstrings(String s1, String s2){
+		return s1 + s2;
+	}
+}
